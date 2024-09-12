25452d6cc17caebbc6076526b20a8413c8251300
TariqKichawele <tariqkichawele66@gmail.com>
2024-09-12 19:43:43 -0400

    Finished implementing authControllers

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,9 +1,91 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res) => {
-    res.send('Welcome')
+    try {
+        const { name, username, email, password } = req.body;
+
+        if(!name || !username || !password || !email) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if(existingEmail) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        const existingUsername = await User.findOne({ username });
+        if(existingUsername) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+
+        if(password.length < 6) {
+            return res.status(400).json({ message: "Password should be at least 6 characters long" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({ name, username, email, password: hashedPassword });
+
+        await user.save();
+
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+
+        res.cookie('jwt-linkedin', token, {
+            httpOnly: true, // prevent XSS attack
+			maxAge: 3 * 24 * 60 * 60 * 1000,
+			sameSite: "strict", // prevent CSRF attacks,
+			secure: process.env.NODE_ENV === "production",
+        })
+
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        console.log("Error in signup: ", error.message);
+		res.status(500).json({ message: "Internal server error" });
+    }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
 
-export const logout = async (req, res) => {};
+        const user = await User.findOne({ username });
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
 
-export const getCurrentUser = async (req, res) => {};
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+        await res.cookie('jwt-linkedin', token, {
+            httpOnly: true, // prevent XSS attack
+            maxAge: 3 * 24 * 60 * 60 * 1000,
+            sameSite: "strict", // prevent CSRF attacks,
+            secure: process.env.NODE_ENV === "production",
+        });
+
+        res.json({ message: "User logged in successfully" });
+    } catch (error) {
+        console.error("Error in login controller:", error);
+		res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const logout = async (req, res) => {
+    res.clearCookie('jwt-linkedin');
+    res.json({ message: "User logged out successfully" });
+};
+
+export const getCurrentUser = async (req, res) => {
+    try {
+        res.json(req.user);
+    } catch (error) {
+        console.error("Error in getCurrentUser controller:", error);
+		res.status(500).json({ message: "Server error" });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
 import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 
 dotenv.config();
@@ -11,7 +12,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use('api/v1/auth', authRoutes);
+app.use(express.json({ limit: '5mb' }));
+app.use(cookieParser())
+
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
