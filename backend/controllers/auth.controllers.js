498e43add40b7c5963a64855cdebd1cec8cce19f
TariqKichawele <tariqkichawele66@gmail.com>
2024-09-12 19:26:28 -0400

    Implemented auth routes and controllers. set up Mongodb

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+    res.send('Welcome')
+};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
+
+export const getCurrentUser = async (req, res) => {};
