51f8d9822bd5c35175cc1ef4420008c6f8849dce
TariqKichawele <tariqkichawele66@gmail.com>
2024-09-13 18:20:11 -0400

    Implemented vite app and set up react-router-dom.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js';
@@ -9,13 +10,20 @@ import notificationRoutes from './routes/notification.routes.js';
 import connectionRoutes from './routes/connection.routes.js';
 
 import { connectDB } from './lib/db.js';
-import cookieParser from 'cookie-parser';
-
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(
+        cors({
+            origin: 'http://localhost:5173',
+            credentials: true
+        })
+    );
+}
 
 app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser())
