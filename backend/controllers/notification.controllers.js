36c95bc558aae1339bf95d03af75cb8a057a7f1f
TariqKichawele <tariqkichawele66@gmail.com>
2024-09-13 12:25:53 -0400

    Implemented Notification routes, controllers and model

--- backend/controllers/notification.controllers.js
+++ backend/controllers/notification.controllers.js
@@ -1,7 +1,49 @@
-export const getUserNotifications = async () => {};
+import Notification from "../models/notification.model.js";
+
+export const getUserNotifications = async (req, res) => {
+    try {
+        const notifications = await Notification.find({ recipient: req.user._id})
+            .sort({ createAt: -1 })
+            .populate("relatedUser", "name username profilePicture")
+            .populate("relatedPost", "content image");
+
+        res.status(200).json(notifications);
+    } catch (error) {
+        console.error("Error in getUserNotifications controller:", error);
+		res.status(500).json({ message: "Internal server error" });
+    }
+};
 
 
-export const markNotificationsAsRead = async () => {};
+export const markNotificationsAsRead = async (req, res) => {
+    const notificationId = req.params.id;
+
+    try {
+        const notification = await Notification.findByIdAndUpdate(
+            { _id: notificationId, recipient: req.user._id },
+            { read: true },
+            { new: true }
+        );
+
+        res.json(notification);
+    } catch (error) {
+        console.error("Error in markNotificationAsRead controller:", error);
+		res.status(500).json({ message: "Internal server error" });
+    }
+};
 
 
-export const deleteNotification = async () => {};
+export const deleteNotification = async (req, res) => {
+    const notificationId = req.params.id;
+
+    try {
+        await Notification.findByIdAndDelete({
+            _id: notificationId, 
+            recipient: req.user._id 
+        });
+        res.json({ message: "Notification deleted successfully" });
+    } catch (error) {
+        console.error("Error in deleteNotification controller:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
